95bae25586721baddcc580b6b526d4317b99fffc
Alius Petraska <alius.petraska@ibm.com>
2017-09-19 11:55:11 +0300

    initial commit;

---
+++ DotNetSummitWebApi/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetSummitWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			var config = new ConfigurationBuilder()
+				.AddCommandLine(args)
+				.Build();
+
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseConfiguration(config)
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
+        }
+    }
+}
